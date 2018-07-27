cfc1aa0b7622f906b937eca84ae128323ad53ee8
Ryo Ota <nwtgck@gmail.com>
2018-07-27 22:46:59 +0900

    [add] Add simple bench to compare between zlib and zstd

---
+++ files/download.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+# (from: http://sun.aei.polsl.pl/~sdeor/index.php?page=silesia)
+curl http://sun.aei.polsl.pl/~sdeor/corpus/dickens.bz2 | bzip2 -d > dickens
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,7 @@ import java.nio.file.Path
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{FileIO, Sink, Source}
+import akka.stream.scaladsl.{Compression, FileIO, Sink, Source}
 import akka.util.ByteString
 import io.github.nwtgck.akka_stream_zstd.ZstdFlow
 
@@ -12,6 +12,12 @@ import scala.concurrent.duration._
 
 object Main {
   def main(args: Array[String]): Unit = {
+    example1()
+
+    runBenchMark()
+  }
+
+  def example1(): Unit = {
     implicit val system: ActorSystem = ActorSystem("my-system")
     implicit val materializer: ActorMaterializer = ActorMaterializer()
     import system.dispatcher
@@ -46,7 +52,93 @@ object Main {
 
     println(s"Decompressed: '${decompressedStr}'")
     system.terminate()
+  }
+
+  // Calculate and print simple benchmark
+  def printSimpleBenchmark(name: String, times: Int = 1)(f: => Unit): Unit = {
+    println(s"================= ${name} =================")
+    val passedTimes: Seq[Long] =
+      for(_ <- 1 to times) yield {
+        val start = System.currentTimeMillis()
+        f
+        val end = System.currentTimeMillis()
+        end - start
+      }
+    println(s"Time: ${passedTimes.sum.toDouble / times} msec")
+
+  }
+
+  def runBenchMark(): Unit = {
+    implicit val system: ActorSystem = ActorSystem("my-system")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    import system.dispatcher
+
+    {
+      val filePath: Path = new File("./files/out.zst").toPath
+
+      printSimpleBenchmark("zstd compress", times = 10) {
+        // Create a simple source
+        val source = FileIO.fromPath(new File("./files/dickens").toPath)
+        // Future of store
+        val storingFut = source
+          // Compress
+          .via(ZstdFlow.compress())
+          // Store to file
+          .runWith(FileIO.toPath(filePath))
+
+        // Wait for store
+        Await.ready(storingFut, 3.seconds)
+      }
 
 
+      printSimpleBenchmark("zstd decompress", times = 10) {
+        // Future of restoring
+        val restoringFut = FileIO.fromPath(filePath)
+          // Decompress
+          .via(ZstdFlow.decompress())
+          // Concatenate into one ByteString
+          .runWith(Sink.fold(ByteString.empty)(_ ++ _))
+          // Convert ByteString to String
+          .map(_.utf8String)
+
+        // Wait for restring
+        Await.ready(restoringFut, 3.seconds)
+      }
+    }
+
+    {
+      val filePath: Path = new File("./files/out.gzip").toPath
+
+      printSimpleBenchmark("zlib compress", times = 10) {
+        // Create a simple source
+        val source = FileIO.fromPath(new File("./files/dickens").toPath)
+        // Future of store
+        val storingFut = source
+          // Compress
+          .via(Compression.gzip)
+          // Store to file
+          .runWith(FileIO.toPath(filePath))
+
+        // Wait for store
+        Await.ready(storingFut, 3.seconds)
+      }
+
+
+      printSimpleBenchmark("zlib decompress", times = 10) {
+        // Future of restoring
+        val restoringFut = FileIO.fromPath(filePath)
+          // Decompress
+          .via(Compression.deflate)
+          // Concatenate into one ByteString
+          .runWith(Sink.fold(ByteString.empty)(_ ++ _))
+          // Convert ByteString to String
+          .map(_.utf8String)
+
+        // Wait for restring
+        Await.ready(restoringFut, 3.seconds)
+      }
+    }
+
+    system.terminate()
   }
 }
